bb6e27dd9c29fe0b9e519f6aa26c6e7cb574a757
eprelofcdesktop <eprelofcdesktop@am.com>
2018-07-17 11:40:17 +0800

    added kubernetes deployment and service yaml files, and a sample of the correct way to create kubernetes secret

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -3,9 +3,14 @@ const fs = require("fs"),
 module.exports = {
   // Get a secret from its name
   get(secret){
-    try{
-      // Swarm secret are accessible within tmpfs /run/secrets dir
+    try {
+      // Kubernetes and Swarm secret are accessible within /run/secrets dir
       return fs.readFileSync(util.format("/run/secrets/%s", secret), "utf8").trim();
+      // From tutorials, Kubernetes secret are accessible within /etc/secret-volume dir
+      // but this path can be change to any path you want. As for my app, I used
+      // /run/secrets folder so that my secrets is accessible in /run/secrets folder
+      // and this get(secret) function will work on both kubernetes or swarm
+      // return fs.readFileSync(util.format("/etc/secret-volume/%s", secret), "utf8").trim();
      }
      catch(e){
        return false;
